88633a08425a47a2b102cef71a0a5302b50cd6de
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-08-25 04:43:15 +0200

    Added error validation

--- PartyInvitation2/Models/GuestResponse.cs
+++ PartyInvitation2/Models/GuestResponse.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvitation2.Models
 {
     
         public class GuestResponse
         {
+        [Required(ErrorMessage = "Please enter your name")]
             public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your email")]
+        // [RegularExpression(".+\\@.+\\.+", ErrorMessage ="Pelase enter a valid email address")]
             public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your phone number")]
             public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Please choose wether you will attend")]
             public bool? WillAttend { get; set; }
        }
     
