a720bfba36cfc689ad36efdb89062cfdc2758c8a
Kedi Kalaj <93157449+kedikalaj@users.noreply.github.com>
2022-08-25 02:08:36 +0200

    Add project files.

---
+++ PartyInvitation2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvitation2.Models;
+
+namespace PartyInvitation2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
+        }
+
+    }
+}
---
+++ PartyInvitation2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PartyInvitation2.MvcApplication" Language="C#" %>
